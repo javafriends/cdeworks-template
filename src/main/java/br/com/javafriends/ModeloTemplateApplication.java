29e4d3657d80bb08cbdf5b1dc1d63752fdc04057
Daniel <javafriendsbr@gmail.com>
2016-09-18 17:38:47 -0300

    first commit

---
+++ src/main/java/br/com/javafriends/ModeloTemplateApplication.java
@@ -0,0 +1,12 @@
+package br.com.javafriends;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModeloTemplateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ModeloTemplateApplication.class, args);
+	}
+}
