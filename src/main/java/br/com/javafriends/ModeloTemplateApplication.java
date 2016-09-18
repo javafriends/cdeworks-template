6f2ab6a9264025e5fab90ad5ee3adbc8513394b5
Daniel <javafriendsbr@gmail.com>
2016-09-18 19:50:35 -0300

    Initial import

--- src/main/java/br/com/javafriends/ModeloTemplateApplication.java
+++ src/main/java/br/com/javafriends/ModeloTemplateApplication.java
@@ -2,11 +2,19 @@ package br.com.javafriends;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
+@Controller
 public class ModeloTemplateApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ModeloTemplateApplication.class, args);
+	}
+	
+	@RequestMapping("/")
+	public String home() {
+		return "index";
 	}
 }
