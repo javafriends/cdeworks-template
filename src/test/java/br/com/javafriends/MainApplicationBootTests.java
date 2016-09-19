93acb4ef35dfa64b6149e4653f26d0210eb83e5d
Daniel <daniel@javafriends.com.br>
2016-09-18 21:43:50 -0300

    Initial import.

--- src/test/java/br/com/javafriends/MainApplicationBootTests.java
+++ src/test/java/br/com/javafriends/MainApplicationBootTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class ModeloTemplateApplicationTests {
+public class MainApplicationBootTests {
 
 	@Test
 	public void contextLoads() {
